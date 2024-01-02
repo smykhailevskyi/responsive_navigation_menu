407630e842135b161a8e07824e985317b357b7c2
Serhii Mykhailevskyi <serhii.mykhailevskyi@gmail.com>
2024-01-02 17:23:37 +0200

    Implement open / hide menu burger functional

--- js/script.js
+++ js/script.js
@@ -17,6 +17,12 @@ const onMenuLinkClick = (e) => {
 
     const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector("header").offsetHeight;
 
+    if(iconMenu.classList.contains("_active")) {
+      document.body.classList.remove("_lock");
+      iconMenu.classList.remove("_active");
+      menuBody.classList.remove("_active");
+    } 
+
     window.scrollTo({
       top: gotoBlockValue,
       behavior: "smooth",
@@ -69,5 +75,17 @@ const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
 if(menuLinks.length > 0) {
   menuLinks.forEach(menuLink => {
     menuLink.addEventListener("click", onMenuLinkClick);
+  });
+}
+
+// Burger menu
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+
+if(iconMenu) {
+  iconMenu.addEventListener("click", function(e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
   });
 }
