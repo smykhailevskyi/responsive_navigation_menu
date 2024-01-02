64f552b62e2311ac72daf9f925a42fddb0ea0580
Serhii Mykhailevskyi <serhii.mykhailevskyi@gmail.com>
2024-01-02 10:35:36 +0200

    Implement smoothing scrolling after menu link click

--- js/script.js
+++ js/script.js
@@ -8,6 +8,22 @@ const addActiveClassForParentElement = (arr) => {
   });
 }
 
+const onMenuLinkClick = (e) => {
+  e.preventDefault();
+  const link = e.target;
+
+  if(link.dataset.goto && document.querySelector(link.dataset.goto)) {
+    const gotoBlock = document.querySelector(link.dataset.goto);
+
+    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector("header").offsetHeight;
+
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth",
+    });
+  }
+}
+
 const isMobile = {
   Android: function() {
     return navigator.userAgent.match(/Android/i);
@@ -45,4 +61,13 @@ if(isMobile.any()) {
   }
 } else {
   document.body.classList.add("_pc");
+}
+
+// Scroll on click
+const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+
+if(menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
 }
